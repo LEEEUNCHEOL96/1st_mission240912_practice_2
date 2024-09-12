370ae7698bc87ed5cc1c2b023158aa416e5e38be
LEEEUNCHEOL <dmscjf1020@naver.com>
2024-09-12 19:38:50 +0900

    btn생성,create맵핑,formClass설정,service수정,createpost맵핑,form.html수정

--- src/main/java/com/example/demo/Article/ArticleService.java
+++ src/main/java/com/example/demo/Article/ArticleService.java
@@ -3,6 +3,7 @@ package com.example.demo.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,15 +12,23 @@ import java.util.Optional;
 public class ArticleService {
     private final ArticleRepository articleRepository;
 
-    public List<Article> getList(){
+    public List<Article> articleList(){
         return  this.articleRepository.findAll();
     }
 
-    public Article getArticle(Integer id) {
+    public Article getList(Integer id) {
         Optional<Article> optionalArticle = this.articleRepository.findById(id);
         if (optionalArticle.isEmpty()){
             return null;
         }
         return optionalArticle.get();
+    }
+
+    public void create(String title, String content){
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        this.articleRepository.save(article);
     }
 }
