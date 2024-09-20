5e346fede2679118af7fd1482c366439d02e8a9e
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-20 13:49:42 +0900

    로그인 화면 구현

--- src/main/java/com/example/demo/User/UserRepository.java
+++ src/main/java/com/example/demo/User/UserRepository.java
@@ -2,6 +2,4 @@ package com.example.demo.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<SiteUser,Long> {
+public interface UserRepository extends JpaRepository<SiteUser,Long> {}
